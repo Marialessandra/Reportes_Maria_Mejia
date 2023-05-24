aa2486e7ec84748f4492c3783d3fff621764b698
Marialessandra <112411969+Marialessandra@users.noreply.github.com>
2023-05-24 16:41:31 -0600

    El proyecto xd

---
+++ Reportes/Reportes/formularios/frmVistaPrevia.vb
@@ -0,0 +1,14 @@
+﻿Public Class frmVistaPrevia
+    Sub verReporte(ByVal t As DataTable, ByVal ds As String, ByVal nombreRpt As String)
+        Try
+            Dim rpt As New ReportDataSource(ds, t)
+            frmVistaPrevia.ReportViewer1.LocalReport.DataSources.Clear()
+            frmVistaPrevia.ReportViewer1.LocalReport.DataSources.Add(rpt)
+            frmVistaPrevia.ReportViewer1.LocalReport.ReportPath = nombreRpt
+            frmVistaPrevia.ReportViewer1.Refresh()
+            frmVistaPrevia.Show()
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Critical, "Error al mostrar reporte")
+        End Try
+    End Sub
+End Class
